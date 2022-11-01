93b1fb50110aa0f41998af44f92e3ddb8cc678c0
Anthony Stevens <aastevens1126@gmail.com>
2022-11-01 09:05:00 -0500

    sql db update

--- albums_migration.sql
+++ albums_migration.sql
@@ -11,3 +11,15 @@ CREATE TABLE IF NOT EXISTS albums (
     genre VARCHAR(100),
     PRIMARY KEY(id)
 );
+
+alter table albums
+add unique(artist,album_name);
+
+describe albums;
+
+select *
+from albums;
+
+INSERT INTO albums (artist, album_name, release_date, sales, genre)
+    VALUE
+    ('Michael Jackson','Thriller',1982,50.2,'Pop, post-disco, funk, rock');
